02cd8403682d5cdefaafe9c9883d8bf7cac30e4b
newExercise <2890053846@qq.com>
2017-11-06 19:54:09 +0800

    20171106 新建的文件夹

---
+++ File/file.mk
@@ -0,0 +1,5 @@
+http://blog.sina.com.cn/s/blog_4c8b519d0101pycd.html
+
+
+github页面上没有明显的按钮提示创建文件夹，谷歌之，发现之间在 创建文件名时键入/即可，so easy。
+参考：http://webapps.stackexchange.com/questions/36411/create-a-folder-in-github-via-the-web-interface
